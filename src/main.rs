054c1cc9a09807268b05094c691ffa4ca073c9ff
Olivier <olivier@Oliviers-MacBook-Pro.local>
2023-03-09 03:44:34 +0000

    Test webhook

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello Olivier")
+    HttpResponse::Ok().body("Hello Olivier!!")
 }
 
 #[post("/echo")]
